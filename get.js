4d443e9fc510d1da2f34811bc8ddd34e914f166b
anhhao00777 <106489200+anhhao00777@users.noreply.github.com>
2024-06-09 18:46:50 +0700

    Update get.js

--- get.js
+++ get.js
@@ -1,11 +1,12 @@
 import * as s1 from './js/file.js';
 import {FileLoader} from 'three';
 
-// golal
+// golal from module
 
 window._start_time_ = Date.now();
 window.file = s1;
 window.ThreeLoader = FileLoader;
+// after gobal, start main script
 var js = document.createElement("script");
 js.src = "js/work.js";
 document.body.appendChild(js);
